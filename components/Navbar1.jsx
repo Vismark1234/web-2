36e76502f702161bf2c30242221bbf71ab84b5fe
Vismark1234 <129228402+Vismark1234@users.noreply.github.com>
2024-12-12 10:20:49 -0400

    Update Navbar1.jsx

--- components/Navbar1.jsx
+++ components/Navbar1.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom"; // Importar Link para navegación interna
 import "./navbar1.css"; // Archivo de estilos externo
 
 const Navbar1 = () => {
@@ -16,27 +17,27 @@ const Navbar1 = () => {
       <div className={`overlay ${menuOpen ? "active" : ""}`} onClick={toggleMenu}>
         <ul className="navbar-menu" onClick={(e) => e.stopPropagation()}>
           <li>
-            <a href="/web-2/">Inicio</a>
+            <Link to="/web-2/">Inicio</Link>
           </li>
           <li>
-            <Link to="/menu">Menu</a>
+            <Link to="/web-2/menu">Menu</Link>
           </li>
           <li>
-            <a href="/web-2/reservas">Reservas</a>
+            <Link to="/web-2/reservas">Reservas</Link>
           </li>
           <li>
-            <a href="/web-2/ubicacion">Ubicacion</a>
+            <Link to="/web-2/ubicacion">Ubicacion</Link>
           </li>
           <li className="dropdown">
-            <a href="/web-2/galeria">Galeria</a>
+            <Link to="/web-2/galeria">Galeria</Link>
             <ul className="dropdown-menu">
-              <li><a href="#subopcion1">Subopción 1</a></li>
-              <li><a href="#subopcion2">Subopción 2</a></li>
-              <li><a href="#subopcion3">Subopción 3</a></li>
+              <li><Link to="#subopcion1">Subopción 1</Link></li>
+              <li><Link to="#subopcion2">Subopción 2</Link></li>
+              <li><Link to="#subopcion3">Subopción 3</Link></li>
             </ul>
           </li>
           <li>
-            <a href="/web-2/nosotros">Nosotros</a>
+            <Link to="/web-2/nosotros">Nosotros</Link>
           </li>
         </ul>
       </div>
